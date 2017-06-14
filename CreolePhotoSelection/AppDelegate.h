0ff0f2789a397a86aab635695705b06eb0c920aa
Nidhi Patel <nidhi@creolestudios.com>
2017-06-14 12:25:55 +0530

    Initial Commit

---
+++ CreolePhotoSelection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CreolePhotoSelection
+//
+//  Created by Nidhi on 6/14/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CreolePhotoSelection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CreolePhotoSelection
+//
+//  Created by Nidhi on 6/14/17.
+//  Copyright © 2017 CreoleStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
